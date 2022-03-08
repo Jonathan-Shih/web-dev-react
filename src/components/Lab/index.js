021a6a6b65dfd31f72d8d9e7f9faa369e62416ec
Jonathan-Shih <jonathan25shih@gmail.com>
2022-03-08 13:22:59 -0500

    a6 lab first commit

---
+++ src/components/Lab/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+
+const Lab = () => {
+    return(
+        <>
+            <h1>Labs</h1>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+        </>
+    )
+};
+
+export default Lab;
