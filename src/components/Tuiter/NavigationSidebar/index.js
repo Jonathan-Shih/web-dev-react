0be7b2b2dc711f977396d3f655045933bf6e2b5d
Jonathan-Shih <jonathan25shih@gmail.com>
2022-04-12 02:04:47 -0400

    rename old dir

--- src/components/Tuiter/NavigationSidebar/index.js
+++ src/components/Tuiter/NavigationSidebar/index.js
@@ -1,18 +1,20 @@
 import React from "react";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import NavigationItem from "./navigation-item";
 
 const NavigationSidebar = () => {
-    const nav = useSelector(state => state.nav);
-    return (
-        <div>
-            <div className="list-group">
-                {
-                    nav.map(item => <NavigationItem navItem={item} key={item.name}/>)
-                }
-            </div>
-            <button type="submit" className="btn w-100 mt-3 btn-primary rounded-pill">Tuit</button>
-        </div>
-    );
-}
+  const nav = useSelector((state) => state.nav);
+  return (
+    <div>
+      <div className="list-group">
+        {nav.map((item) => (
+          <NavigationItem navItem={item} key={item.name} />
+        ))}
+      </div>
+      <button type="submit" className="btn w-100 mt-3 btn-primary rounded-pill">
+        Tuit
+      </button>
+    </div>
+  );
+};
 export default NavigationSidebar;
